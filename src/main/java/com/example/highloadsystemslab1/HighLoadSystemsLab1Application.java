513f0baaf85819d661e8a06167e6db525728763a
Polina Popova <cherrywood6918@gmail.com>
2022-09-05 18:10:15 +0300

    Initial commit

---
+++ src/main/java/com/example/highloadsystemslab1/HighLoadSystemsLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.highloadsystemslab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighLoadSystemsLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighLoadSystemsLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/highloadsystemslab1/HighLoadSystemsLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.highloadsystemslab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighLoadSystemsLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
