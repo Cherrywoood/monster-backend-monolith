4b29d9380b4fddb3028255ddb4eeba0275a7a9ce
Polina Popova <cherrywood6918@gmail.com>
2022-09-13 20:27:12 +0300

    feat: add user and role dto

---
+++ src/main/java/ru/itmo/monsters/model/dto/RoleDTO.java
@@ -0,0 +1,13 @@
+package ru.itmo.monsters.model.dto;
+
+public class RoleDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
