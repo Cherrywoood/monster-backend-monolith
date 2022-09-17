ded5048bd50ba189993e7f02a0b8b2842c5ef676
Polina Popova <cherrywood6918@gmail.com>
2022-09-17 22:18:36 +0300

    refactor: rename config file

--- src/main/java/ru/itmo/monsters/config/WebConfig.java
+++ src/main/java/ru/itmo/monsters/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class WebConfig {
 
     @Bean
     public ModelMapper modelMapper() {
