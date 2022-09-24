56970cd7fbd6b9db9341cc0aa37011bd00af698e
Polina Popova <cherrywood6918@gmail.com>
2022-09-25 01:58:14 +0300

    refactor: rename mapper class

--- src/main/java/ru/itmo/monsters/mapper/RoleMapper.java
+++ src/main/java/ru/itmo/monsters/mapper/RoleMapper.java
@@ -1,20 +1,21 @@
-package ru.itmo.monsters.converter;
+package ru.itmo.monsters.mapper;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import ru.itmo.monsters.dto.RoleDTO;
 import ru.itmo.monsters.model.RoleEntity;
+
 @RequiredArgsConstructor
 @Component
-public class RoleConverter {
+public class RoleMapper {
     private final ModelMapper modelMapper;
 
-    public RoleDTO convertEntityToDto(RoleEntity roleEntity) {
+    public RoleDTO mapEntityToDto(RoleEntity roleEntity) {
         return modelMapper.map(roleEntity, RoleDTO.class);
     }
 
-    public RoleEntity convertDtoToEntity(RoleDTO roleDTO) {
+    public RoleEntity mapDtoToEntity(RoleDTO roleDTO) {
         return modelMapper.map(roleDTO, RoleEntity.class);
     }
 }
