2d82e380469dc22e936559d614b340cbdd3c8834
Polina Popova <cherrywood6918@gmail.com>
2022-09-25 15:23:27 +0300

    feat: add repositories

---
+++ src/main/java/ru/itmo/monsters/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package ru.itmo.monsters.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.itmo.monsters.model.RoleEntity;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
+    Optional<RoleEntity> findByName(String name);
+}
---
+++ src/main/java/ru/itmo/monsters/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package ru.itmo.monsters.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.itmo.monsters.model.UserEntity;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByLogin(String login);
+
+
+}
