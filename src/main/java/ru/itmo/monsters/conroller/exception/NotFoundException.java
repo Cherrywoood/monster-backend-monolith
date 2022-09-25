43ff5beebe945851ab9a2f349fff88ff4f845189
Polina Popova <cherrywood6918@gmail.com>
2022-09-25 19:10:10 +0300

    feat: add custom exception

---
+++ src/main/java/ru/itmo/monsters/conroller/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package ru.itmo.monsters.conroller.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
