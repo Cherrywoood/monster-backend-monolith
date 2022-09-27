02688f4c524269953550805ab70ded25762dacdb
Ekaterina <ekaterina.2001@live.com>
2022-09-27 22:59:48 +0300

    feat: make enums in separate package

---
+++ src/main/java/ru/itmo/monsters/enums/Gender.java
@@ -0,0 +1,7 @@
+package ru.itmo.monsters.enums;
+
+public enum Gender {
+    MALE, FEMALE
+}
+
+
---
+++ src/main/java/ru/itmo/monsters/enums/Job.java
@@ -0,0 +1,5 @@
+package ru.itmo.monsters.enums;
+
+public enum Job {
+    SCARER, CLEANER, SCARE_ASSISTANT, DISINFECTOR, RECRUITER
+}
