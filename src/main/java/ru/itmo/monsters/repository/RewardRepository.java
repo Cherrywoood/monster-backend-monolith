51955c1f594ec2196a50ef1b18ceb2e2c8d1900c
Ekaterina <ekaterina.2001@live.com>
2022-10-09 13:14:29 +0300

    feat: add repositories

--- src/main/java/ru/itmo/monsters/repository/RewardRepository.java
+++ src/main/java/ru/itmo/monsters/repository/RewardRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.itmo.monsters.model.RewardEntity;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RewardRepository extends JpaRepository<RewardEntity, UUID> {
+    Optional<RewardEntity> findByMoney(int money);
 }
