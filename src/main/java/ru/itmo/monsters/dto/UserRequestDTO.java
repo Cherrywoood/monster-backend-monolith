43828d1c894a4c9c6ef66ffd225c7ced7ff8e945
Polina Popova <cherrywood6918@gmail.com>
2022-10-11 01:02:02 +0300

    feat: split user dto into response and request

--- src/main/java/ru/itmo/monsters/dto/UserRequestDTO.java
+++ src/main/java/ru/itmo/monsters/dto/UserRequestDTO.java
@@ -1,25 +1,23 @@
 package ru.itmo.monsters.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.UUID;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class UserDTO {
-    private UUID id;
+@AllArgsConstructor
+@Builder
+public class UserRequestDTO {
 
     @NotBlank(message = "cannot be null, empty or whitespace")
     @Size(min = 4, max = 16, message = "must be between 4 and 16 characters")
     private String login;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotBlank(message = "cannot be null, empty or whitespace")
     @Size(min = 5, message = "must be more than 5 characters")
     private String password;
@@ -27,4 +25,6 @@ public class UserDTO {
     @NotBlank(message = "cannot be null, empty or whitespace")
     @Size(min = 4, max = 16, message = "must be between 4 and 16 characters")
     private String role;
+
+
 }
