e50b2c5b0be2503fb460b7554e7e9d197abb3c4e
Polina Popova <cherrywood6918@gmail.com>
2022-10-11 00:44:48 +0300

    feat: аdd insert script

---
+++ src/main/resources/db/changelog/v2/insertAdmin.sql
@@ -0,0 +1,6 @@
+INSERT INTO ROLES
+VALUES ('ccf8cd50-b9fc-4e63-8b5c-6327269adfc9', 'ADMIN');
+INSERT INTO USERS
+VALUES ('7e73fbee-dab5-4baf-93d6-121690ec9263', 'polina',
+        '$2a$12$kXVaFigFyGR2ta3OUkyzfOanmHLHvHY0jtpbxGlSkNc91MTqTG52W',
+        'ccf8cd50-b9fc-4e63-8b5c-6327269adfc9');
---
+++ src/main/resources/db/changelog/v3/insertUsers.sql
@@ -0,0 +1,16 @@
+INSERT INTO USERS
+VALUES ('78a87ecf-b365-47e9-99d2-dfdd17fc06b4', 'katya',
+        '$2a$12$kXVaFigFyGR2ta3OUkyzfOanmHLHvHY0jtpbxGlSkNc91MTqTG52W',
+        '2835e28c-06ad-404c-8248-c51fe7cba310'),
+       ('ae72569e-20ea-493d-bbed-b4ac7c1beeb7', 'pimpa',
+        '$2a$12$kXVaFigFyGR2ta3OUkyzfOanmHLHvHY0jtpbxGlSkNc91MTqTG52W',
+        '8c776de5-3f82-4c7d-8ae8-210f13193b52'),
+       ('4ae07ce0-f528-4f2f-8b35-097a90e21993', 'amogus',
+        '$2a$12$kXVaFigFyGR2ta3OUkyzfOanmHLHvHY0jtpbxGlSkNc91MTqTG52W',
+        '074be971-a5d5-449a-97dc-f2cc2bb028ac'),
+       ('54137cda-a1ba-4d1a-9d6d-9d32085cdb6d', 'power',
+        '$2a$12$kXVaFigFyGR2ta3OUkyzfOanmHLHvHY0jtpbxGlSkNc91MTqTG52W',
+        '25b345da-c2f9-4079-842f-bdaac7e44aa5'),
+       ('a5a836ab-8aac-48f4-8ad6-85c8e6692e3e', 'wolf69',
+        '$2a$12$kXVaFigFyGR2ta3OUkyzfOanmHLHvHY0jtpbxGlSkNc91MTqTG52W',
+        'e5cd8334-cb47-4c86-b25e-365227300289');
