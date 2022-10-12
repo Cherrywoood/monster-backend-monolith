db7b8d6699f9b07cf4ebc7c0acb4675929e03e32
Polina Popova <cherrywood6918@gmail.com>
2022-10-12 21:16:59 +0300

    feat: resolve problem n+1

--- src/main/java/ru/itmo/monsters/repository/UserRepository.java
+++ src/main/java/ru/itmo/monsters/repository/UserRepository.java
@@ -2,6 +2,7 @@ package ru.itmo.monsters.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.itmo.monsters.model.RoleEntity;
@@ -15,6 +16,9 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByLogin(String login);
 
     Page<UserEntity> findAllByRole(RoleEntity role, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"role"})
+    Page<UserEntity> findAll(Pageable pageable);
 
 
 }
