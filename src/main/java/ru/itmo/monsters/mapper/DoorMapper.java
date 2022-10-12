3fcec98b76381123d1a205d66a593d1fa47cc782
polinadelaet <polina.usui@gmail.com>
2022-10-12 14:59:17 +0300

    feat: add DoorMapper

---
+++ src/main/java/ru/itmo/monsters/mapper/DoorMapper.java
@@ -0,0 +1,25 @@
+package ru.itmo.monsters.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.itmo.monsters.dto.DoorDTO;
+import ru.itmo.monsters.model.DoorEntity;
+
+@RequiredArgsConstructor
+@Component
+public class DoorMapper {
+
+    public DoorDTO mapEntityToDto(DoorEntity doorEntity) {
+        return DoorDTO.builder()
+                .id(doorEntity.getId())
+                .is_active(doorEntity.isActive())
+                .build();
+    }
+
+    public DoorEntity mapDtoToEntity(DoorDTO doorDTO) {
+        return DoorEntity.builder()
+                .id(doorDTO.getId())
+                .isActive(doorDTO.is_active())
+                .build();
+    }
+}
