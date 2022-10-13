d854bf18b1dd54f69b55b1cc17bd2c8b37dbd792
Polina Popova <cherrywood6918@gmail.com>
2022-10-13 19:47:51 +0300

    feat: n+1 resolve

--- src/main/java/ru/itmo/monsters/repository/InfectionRepository.java
+++ src/main/java/ru/itmo/monsters/repository/InfectionRepository.java
@@ -2,6 +2,7 @@ package ru.itmo.monsters.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.itmo.monsters.model.InfectionEntity;
@@ -12,4 +13,7 @@ import java.util.UUID;
 @Repository
 public interface InfectionRepository extends JpaRepository<InfectionEntity, UUID> {
     Page<InfectionEntity> findAllByMonster(MonsterEntity monster, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"infectedThing","monster"})
+    Page<InfectionEntity> findAll(Pageable pageable);
 }
